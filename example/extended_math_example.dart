e480a34f398734e2d5445765bd8ad495f3a5787b
danrosselli <daniel@soware.com.br>
2023-12-13 17:02:59 -0300

    update to dart 3.0.0 null safety

--- example/extended_math_example.dart
+++ example/extended_math_example.dart
@@ -4,40 +4,52 @@ void main() {
   final v1 = Vector(<double>[1, 2, 3]);
   final v2 = Vector(<double>[4, 5, 6]);
   // Multiply vectors
-  final double res = v1.dot(v2);
+  final num res = v1.dot(v2);
+  print(res);
+
   // Add vectors
   final Vector res1 = v1 + v2;
+  print(res1);
 
   final v5 = Vector(<double>[6, 3]);
   final v6 = Vector(<double>[5, 13]);
+
   // Gets angle between two vectors
   print(v5.angleBetween(v6));
 
   final v3 = SquareMatrix(<List<double>>[
-    <double>[9, 3, 5], 
-    <double>[-6, -9, 7], 
+    <double>[9, 3, 5],
+    <double>[-6, -9, 7],
     <double>[-1, -8, 1]
   ]);
+
   // Gets determinant of matrix
-  final double det = v3.determinant();
+  final num det = v3.determinant();
+  print(det);
 
   // Computes eigenvalues and eigenvectors of square matrix
   final Map<num, Vector> result = v3.eigen();
+  print(result);
 
   final m = Matrix(<List<double>>[
     <double>[2, -1, 5],
     <double>[0, 2, 1],
     <double>[3, 1, 1]
   ]);
+
   // Computes Frobenius norm of matrix
   print(m.frobeniusNorm());
 
   final c = Complex(re: -4);
   final c2 = Complex(re: -4);
+
   // Gets root of complex number
   final r = c.rootsOf(2);
+  print(r);
+
   // Divides complex numbers
   print(c / c2);
+
   // Gets pow of complex number
   print(Complex(re: 2).pow(2));
 }
